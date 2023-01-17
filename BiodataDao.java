1157744c6180297e28ef81622fb2c7fface48912
poonam nayakodi <90123591+nayakodipoonam@users.noreply.github.com>
2023-01-17 16:36:19 +0530

    Delete BiodataDao.java

--- BiodataDao.java
+++
@@ -1,7 +0,0 @@
-package com.springbootdb.springbootdb.dao;
-
-import com.springbootdb.springbootdb.entities.Biodata;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BiodataDao extends JpaRepository<Biodata, Long> {
-}
